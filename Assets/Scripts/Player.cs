2d84886ec2c3b9c8843747983cca64ef4be19bf2
dz1mka <dzimka@gmail.com>
2025-04-20 21:05:30 +0300

    25 - Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(movementX, 0, 0) * moveForce * Time.deltaTime;
+
+      
+    }
+}
