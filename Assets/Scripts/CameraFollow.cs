a13c3ee03f618f277d9db3df780f259e3ac72348
dz1mka <dzimka@gmail.com>
2025-04-21 22:11:11 +0300

    28 - Camera Follow Player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+
+    private Vector3 tempPos;
+
+    [SerializeField]
+    private float mimX, maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        tempPos = transform.position;
+        tempPos.x = player.position.x;
+
+        if (tempPos.x < mimX)
+        {
+            tempPos.x = mimX;
+        }
+        else if (tempPos.x > maxX)
+        {
+            tempPos.x = maxX;
+        }
+
+        transform.position = tempPos;
+    }
+}
