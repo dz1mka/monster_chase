d1cc54f88f03aafd4de944beffcdd927a0d922b9
dz1mka <dzimka@gmail.com>
2025-06-15 13:05:45 +0300

    42 - Finishing Our Game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -47,7 +47,10 @@ public class GameManager : MonoBehaviour
 
     void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
     {
-
+        if(scene.name == "Gameplay")
+        {
+           Instantiate(characters[CharIndex]);
+        }
     }
 
 }
